1dd13d2f0303872bb661fedbef8ed62d55c5ceb9
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-30 23:27:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,10 @@
 def unsafe?(speed)
-	if speed.between?(40,60)
-		false
-	else
-		speed.between?=40,60
-		if speed>60
-			true
-		else speed<40
-			true
-		end
-	end
+    if speed <40 or speed>60
+        return true
+    else false
+    end
 end
 
 def not_safe?(speed)
-	speed.between?(40,60)? false : true
+    speed <40 or speed>60 ? true : false
 end
